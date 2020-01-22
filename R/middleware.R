77134152e524c667210e63a41864784b7927a97f
gerard0315 <gerardtao0315@gmail.com>
2020-01-22 18:10:42 +0800

    more examples

--- R/middleware.R
+++ R/middleware.R
@@ -267,7 +267,8 @@ routeHandler <- function(prefix, token, handler) {
 
       message("SCRIPT_NAME ", req$SCRIPT_NAME)
 
-      req$PATH_INFO <- paste(req$SCRIPT_NAME, prefix, "/?token=", token, sep = "")
+      req$PATH_INFO <- paste(req$SCRIPT_NAME, prefix, sep = "")
+      # req$PATH_INFO <- paste(req$SCRIPT_NAME, prefix, '/?token=', token, sep = "")
 
       message('\n', 'path prefix attached ', req$PATH_INFO)
       
