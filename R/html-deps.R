610b555d2b063485ed7b94d7a5d2b3673e0b7b26
gerard0315 <gerardtao0315@gmail.com>
2020-02-07 18:26:02 +0800

    message

--- R/html-deps.R
+++ R/html-deps.R
@@ -27,7 +27,7 @@ createWebDependency <- function(dependency, scrubFile = TRUE) {
     stop("Unexpected non-html_dependency type")
 
   if (is.null(dependency$src$href)) {
-    message("\n createWebDependency dependency$src$href ", dependency)
+    message("\n createWebDependency dependency$src$href ")
     prefix <- paste(dependency$name, "-", dependency$version, sep = "")
     addResourcePath(prefix, dependency$src$file)
     dependency$src$href <- prefix
